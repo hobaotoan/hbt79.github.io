f7b0dfdf180a764a2383ae4c5268fe97b7343f04
hobaotoan <65803323+hobaotoan@users.noreply.github.com>
2021-12-17 13:27:25 +0700

    sda

--- B1906785-B1906594/js/personal.js
+++
@@ -1,8 +0,0 @@
-window.onload = function showInfo(){
-    
-    document.getElementById('namePerson').innerHTML = "" + sessionStorage.getItem("username") + "";
-    var person_email = document.getElementById('emailPerson');
-    person_email.innerHTML = "" + sessionStorage.getItem("email") + "";
-    var person_phone = document.getElementById('phonePerson');
-    person_phone.innerHTML = "" + sessionStorage.getItem("phone") + "";
-}
